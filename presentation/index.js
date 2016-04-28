782fb8d904901c91a8b10119f84cc1eb2bd6a2d6
Henry Zhu <hi@henryzoo.com>
2016-04-28 18:23:11 -0400

    more updates

--- presentation/index.js
+++ presentation/index.js
@@ -3,20 +3,11 @@ import React from "react";
 
 // Import Spectacle Core tags
 import {
-  Appear,
-  BlockQuote,
-  Cite,
-  CodePane,
   Deck,
-  Fill,
   Heading,
   Image,
-  Layout,
   Link,
-  ListItem,
-  List,
   Markdown,
-  Quote,
   Slide,
   S,
   Spectacle,
@@ -39,11 +30,15 @@ import Interactive from "../assets/interactive";
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
 
-
 const images = {
   reactTrending: require("../assets/react-trending.png"),
   stackOverflow: require("../assets/stackoverflow-2016.png"),
-  reactHotLoader: require("../assets/react-hot-loader.png")
+  reactHotLoader: require("../assets/react-hot-loader.png"),
+  diff: require("../assets/diff.png"),
+  codemod: require("../assets/codemod.png"),
+  codemod1: require("../assets/codemod1.png"),
+  codemod2: require("../assets/codemod2.png"),
+  reactnative: require("../assets/reactnative.png"),
 };
 
 preloader(images);
@@ -51,6 +46,37 @@ preloader(images);
 const theme = createTheme({
   primary: "#ff4081"
 });
+
+const names = ["Alice", "Emily", "Kate"];
+
+const HelloMessage = (props) => <h1>Hello {props.name}</h1>;
+
+const ValidateHelloMessage = (props) => <h1>Hello {props.name}</h1>;
+
+ValidateHelloMessage.propTypes = {
+  name: React.PropTypes.string.isRequired,
+};
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ liked: !this.state.liked });
+  }
+
+  render() {
+    const text = this.state.liked ? "like" : "haven't liked";
+    return (
+      <p onClick={this.handleClick}>
+        You {text} this. Click to toggle.
+      </p>
+    );
+  }
+}
 
 export default class Presentation extends React.Component {
   render() {
@@ -64,13 +90,14 @@ export default class Presentation extends React.Component {
             <Heading size={2} fit caps lineHeight={1} textColor="black">
               A JAVASCRIPT LIBRARY FOR BUILDING USER INTERFACES
             </Heading>
+            <br />
           </Slide>
           <Slide bgImage={images.stackOverflow.replace("/", "")} bgDarken={0.25}>
             <Image src={images.reactTrending.replace("/", "")} margin="0px auto 350px" height="300px"/>
           </Slide>
-          <Slide notes="commands: append, remove, update, etc">
+          <Slide>
             <Heading size={2} fit caps lineHeight={1} textColor="black">
-              How do we update when data changes?
+              How do we update the UI when data changes?
             </Heading>
           </Slide>
           <Slide transition={["slide"]} bgColor="primary">
@@ -117,6 +144,11 @@ export default class Presentation extends React.Component {
               { loc: [18, 20] },
             ]}
           />
+          <Slide notes="Instead of: How do we update the UI when data changes?">
+            <Heading size={2} fit caps lineHeight={1} textColor="black">
+              What should the UI look like when data changes?
+            </Heading>
+          </Slide>
           <Slide notes="what vs how (instructions), finding a loaf of bread, removing two slices, applying toppings, etc">
             <Link target="_blank" href="https://github.com/facebook/react-devtools">
               <Heading bold caps textColor="tertiary">React is Declarative</Heading>
@@ -190,15 +222,278 @@ export default class Presentation extends React.Component {
               { loc: [12, 13], note: "F(props) = Component(count)" }
             ]}
           />
+          <Slide>
+            <Heading size={2} fit caps lineHeight={1} textColor="black">
+              React abstracts the imperative DOM
+            </Heading>
+            <Image
+              src={images.diff.replace("/", "")}
+            />
+          </Slide>
+          <Slide>
+            <Heading size={2} fit caps lineHeight={1} textColor="black">
+              What about reusability?
+            </Heading>
+          </Slide>
           <Slide notes="Seperate concerns by components rather than by templates or technologies (html, css, js)">
             <Heading size={2} fit caps lineHeight={1} textColor="black">
-              Everything is a Component
+              Everything is a React Component
             </Heading>
+            <br />
+            <Text>What if you could make your own HTML elements (select, h1, img)?</Text>
+            <br />
+            <Heading size={2} fit caps lineHeight={1} textColor="black">
+              import Toolbar from 'components/toolbar';
+            </Heading>
+          </Slide>
+          <CodeSlide
+            transition={[]}
+            lang="js"
+            code={require("raw!../assets/template.example")}
+            ranges={[
+              { loc: [0, 7], note: "no need to learn a new templating syntax" },
+              { loc: [8, 18], title: "You can use anything in JS" },
+            ]}
+          />
+          <Slide>
+            <Heading size={2} fit caps lineHeight={1} textColor="black">
+              React as Library
+            </Heading>
+          </Slide>
+          <CodeSlide
+            transition={[]}
+            lang="js"
+            code={
+`var MyTitle = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string.isRequired,
+  },
+
+  render: function() {
+     return <h1> {this.props.title} </h1>;
+   }
+});
+
+var data = 123;
+ReactDOM.render(
+  <MyTitle title={data} />,
+  document.getElementById('example')
+);
+
+// Warning: Failed propType: Invalid prop 'title' of type 'number' supplied to 'MyTitle', expected 'string'.`}
+            ranges={[
+              { loc: [0, 20], title: "React Proptypes" },
+              { loc: [1, 4], title: "Component Validation" },
+              { loc: [6, 7], note: "make sure title is a string and is required" },
+              { loc: [10, 13], note: "passing a number rather than a string" },
+              { loc: [16, 17], note: "results in a warning" },
+            ]}
+          />
+          <Slide>
+            <Heading size={2} fit caps lineHeight={1} textColor="black">
+              Helpful error messages in development
+            </Heading>
+            <Image
+              src="http://reactkungfu.com/assets/images/upgrading-to-react-014/console1.png"
+            />
+          </Slide>
+          <Slide>
+            <Heading size={2} fit caps lineHeight={1} textColor="black">
+              Same in Upgrading
+            </Heading>
+            <Image
+              src="http://reactkungfu.com/assets/images/upgrading-to-react-014/console2.png"
+            />
+          </Slide>
+          <Slide>
+            <Heading size={2} fit caps lineHeight={1} textColor="black">
+              Automated code modifications for API changes
+            </Heading>
+            <Image
+              src={images.codemod.replace("/", "")}
+            />
+          </Slide>
+          <Slide>
+            <Image
+              src={images.codemod1.replace("/", "")}
+            />
+            <Image
+              src={images.codemod2.replace("/", "")}
+            />
+          </Slide>
+          <Slide notes="would be difficult to lint strings">
+            <Heading size={2} fit caps lineHeight={1} textColor="black">
+              Lint JSX
+            </Heading>
+            <Image
+              src="http://eslint.org/img/logo.svg"
+              width="100px"
+            />
+            <Link target="_blank" href="https://github.com/yannickcr/eslint-plugin-react">
+              <Text>eslint-plugin-react</Text>
+            </Link>
+            <Link target="_blank" href="https://github.com/babel/babel-eslint">
+              <Text>babel-eslint</Text>
+            </Link>
+            <Markdown>
+            {
+              `
+    // jsx-uses-vars'
+    var Hello = require('./Hello'); // warns unused
+
+    ---
+
+    var Hello = require('./Hello'); // no warning
+    <Hello name="John" />;
+              `
+            }
+            </Markdown>
           </Slide>
           <Slide notes="some demos of tools">
             <Heading size={2} fit caps lineHeight={1} textColor="black">
               Some Demos
             </Heading>
+          </Slide>
+          <CodeSlide
+            transition={[]}
+            lang="js"
+            code={
+`ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+);`}
+            ranges={[
+              { loc: [0, 4], note: "render(ReactElement element, DOMElement container)" },
+            ]}
+          />
+          <Slide>
+            <h1>Hello, world!</h1>
+          </Slide>
+          <CodeSlide
+            transition={[]}
+            lang="js"
+            code={
+`var names = ['Alice', 'Emily', 'Kate'];
+
+ReactDOM.render(
+  <div>
+  {
+    names.map(function (name) {
+      return <div>Hello, {name}!</div>
+    })
+  }
+  </div>,
+  document.getElementById('example')
+);`}
+            ranges={[
+              { loc: [0, 13], title: "rendering a list of names" },
+              { loc: [4, 9], note: "any valid javascript expression in {}" },
+            ]}
+          />
+          <Slide>
+            <div>
+            {
+              names.map((name) => <div>Hello, {name}!</div>)
+            }
+            </div>
+          </Slide>
+          <CodeSlide
+            transition={[]}
+            lang="js"
+            code={
+`var HelloMessage = (props) => <h1>Hello {props.name}</h1>;
+
+ReactDOM.render(
+  <HelloMessage name="John" />,
+  document.getElementById('example')
+);`}
+            ranges={[
+              { loc: [0, 13], title: "Defining a Component" },
+            ]}
+          />
+          <Slide>
+            <HelloMessage name="John" />
+          </Slide>
+          <CodeSlide
+            transition={[]}
+            lang="js"
+            code={
+`var ValidateHelloMessage = (props) => <h1>Hello {props.name}</h1>;
+
+ValidateHelloMessage.propTypes = {
+  name: React.PropTypes.string.isRequired,
+};
+ReactDOM.render(
+  <ValidateHelloMessage name="John" />,
+  document.getElementById('example')
+);`}
+            ranges={[
+              { loc: [0, 13], title: "Proptypes" },
+            ]}
+          />
+          <Slide>
+            <ValidateHelloMessage name={1} name="John" />
+          </Slide>
+          <CodeSlide
+            transition={[]}
+            lang="js"
+            code={
+`class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({ liked: !this.state.liked });
+  }
+
+  render() {
+    var text = this.state.liked ? 'like' : 'haven\'t liked';
+    return (
+      <p onClick={this.handleClick}>
+        You {text} this. Click to toggle.
+      </p>
+    );
+  }
+}
+
+ReactDOM.render(
+  <LikeButton />,
+  document.getElementById('example')
+);`}
+            ranges={[
+              { loc: [0, 30], title: "State" },
+              { loc: [0, 1], note: "ES2015 Class syntax" },
+              { loc: [1, 7], note: "setup initial state (false)" },
+              { loc: [11, 20], note: "render method" },
+              { loc: [14, 15], note: "click handler" },
+              { loc: [7, 10], note: "setState -> re-render" },
+            ]}
+          />
+          <Slide>
+            <LikeButton />
+          </Slide>
+          <Slide notes="babel">
+            <Heading size={1} caps fit textColor="tertiary">
+              <Link target="_blank" href="http://babeljs.io/repl/#?evaluate=true&presets=es2015%2Ces2015-loose%2Creact&experimental=false&loose=false&spec=false&code=var%20Constant%20%3D%20%3Cdiv%3EHello%20World%3C%2Fdiv%3E%3B%0A%0Avar%20ES5Component%20%3D%20React.createClass(%7B%0A%20%20render%3A%20function()%20%7B%0A%20%20%20%20return%20%3Cdiv%3EHello%20%7Bthis.props.name%7D%3C%2Fdiv%3E%3B%0A%20%20%7D%0A%7D)%3B%0A%0Aclass%20ES2015ClassComponent%20extends%20React.Component%20%7B%0A%20%20render()%20%7B%0A%20%20%20%20return%20%3Cdiv%3EHello%20%7Bthis.props.name%7D%3C%2Fdiv%3E%3B%0A%20%20%7D%0A%7D%0A%0Afunction%20StatelessFunction(props)%20%7B%0A%20%20return%20%3Cdiv%3EHello%20%7Bprops.name%7D%3C%2Fdiv%3E%3B%0A%7D%0A%0Aconst%20ES2015StatelessFunction%20%3D%20(props)%20%3D%3E%20%3Cdiv%3EHello%20%7Bprops.name%7D%3C%2Fdiv%3E%3B&playground=true">
+                <S type="underline"><Text bold caps textColor="tertiary">Babel</Text></S>
+              </Link>
+            </Heading>
+            <Text>babel-plugin-transform-react-jsx</Text>
+          </Slide>
+          <Slide>
+            <Heading size={1} caps fit textColor="tertiary">
+              <Link target="_blank" href="https://github.com/thejameskyle/babel-react-optimize#babel-react-optimize">
+                <S type="underline"><Text bold caps textColor="tertiary">React Optimizations</Text></S>
+              </Link>
+            </Heading>
+          </Slide>
+          <Slide align="flex-start" notes="show how the slides themselves are hot reloadable, show error messages by typo, etc" bgImage={images.reactHotLoader.replace("/", "")}>
+            <Link target="_blank" href="http://gaearon.github.io/react-hot-loader/">
+              <Text>Link</Text>
+            </Link>
           </Slide>
           <Slide>
             <Link target="_blank" href="https://github.com/facebook/react-devtools">
@@ -212,21 +507,17 @@ export default class Presentation extends React.Component {
               />
             </Link>
           </Slide>
-          <Slide notes="babel">
-            <Heading size={1} caps fit textColor="tertiary">
-              <Link target="_blank" href="http://babeljs.io/repl/#?evaluate=true&presets=es2015%2Ces2015-loose%2Creact&experimental=false&loose=false&spec=false&code=var%20Constant%20%3D%20%3Cdiv%3EHello%20World%3C%2Fdiv%3E%3B%0A%0Avar%20ES5Component%20%3D%20React.createClass(%7B%0A%20%20render%3A%20function()%20%7B%0A%20%20%20%20return%20%3Cdiv%3EHello%20%7Bthis.props.name%7D%3C%2Fdiv%3E%3B%0A%20%20%7D%0A%7D)%3B%0A%0Aclass%20ES2015ClassComponent%20extends%20React.Component%20%7B%0A%20%20render()%20%7B%0A%20%20%20%20return%20%3Cdiv%3EHello%20%7Bthis.props.name%7D%3C%2Fdiv%3E%3B%0A%20%20%7D%0A%7D%0A%0Afunction%20StatelessFunction(props)%20%7B%0A%20%20return%20%3Cdiv%3EHello%20%7Bprops.name%7D%3C%2Fdiv%3E%3B%0A%7D%0A%0Aconst%20ES2015StatelessFunction%20%3D%20(props)%20%3D%3E%20%3Cdiv%3EHello%20%7Bprops.name%7D%3C%2Fdiv%3E%3B&playground=true">
-                <S type="underline"><Text bold caps textColor="tertiary">Babel</Text></S>
-              </Link>
-            </Heading>
-            <Link target="_blank" href="http://babeljs.io/repl/#?evaluate=true&presets=es2015%2Ces2015-loose%2Creact&experimental=false&loose=false&spec=false&code=class%20Interactive%20extends%20React.Component%20%7B%0A%20%20constructor()%20%7B%0A%20%20%20%20super()%3B%0A%20%20%20%20this.state%20%3D%20%7B%0A%20%20%20%20%20%20count%3A%200%0A%20%20%20%20%7D%3B%0A%20%20%20%20this.handleClick%20%3D%20this.handleClick.bind(this)%3B%0A%20%20%7D%0A%20%20handleClick()%20%7B%0A%20%20%20%20this.setState(%7B%0A%20%20%20%20%20%20count%3A%20this.state.count%20%2B%201%0A%20%20%20%20%7D)%3B%0A%20%20%7D%0A%20%20render()%20%7B%0A%20%20%20%20const%20headingStyle%20%3D%20%7B%0A%20%20%20%20%20%20textTransform%3A%20'uppercase'%0A%20%20%20%20%7D%3B%0A%20%20%20%20const%20styles%20%3D%20%7B%0A%20%20%20%20%20%20padding%3A%2020%2C%0A%20%20%20%20%20%20background%3A%20%22black%22%2C%0A%20%20%20%20%20%20minWidth%3A%20300%2C%0A%20%20%20%20%20%20marginTop%3A%2020%2C%0A%20%20%20%20%20%20textTransform%3A%20%22uppercase%22%2C%0A%20%20%20%20%20%20border%3A%20%22none%22%2C%0A%20%20%20%20%20%20color%3A%20%22white%22%2C%0A%20%20%20%20%20%20outline%3A%20%22none%22%2C%0A%20%20%20%20%20%20fontWeight%3A%20%22bold%22%2C%0A%20%20%20%20%20%20fontSize%3A%20%222em%22%0A%20%20%20%20%7D%3B%0A%20%20%20%20return%20(%0A%20%20%20%20%20%20%3Cdiv%3E%0A%20%20%20%20%20%20%7Bthis.state.count%20%3C%205%20%3F%0A%20%20%20%20%20%20%20%20%3Cdiv%3E%0A%20%20%20%20%20%20%20%20%20%20%3Ch1%20style%3D%7BheadingStyle%7D%3E%0A%20%20%20%20%20%20%20%20%20%20%20%20The%20button%20has%20been%20clicked%20%7Bthis.state.count%7D%20times%0A%20%20%20%20%20%20%20%20%20%20%3C%2Fh1%3E%0A%20%20%20%20%20%20%20%20%20%20%3Cbutton%20style%3D%7Bstyles%7D%20type%3D%22button%22%20onClick%3D%7Bthis.handleClick%7D%3EClick%20Me%3C%2Fbutton%3E%0A%20%20%20%20%20%20%20%20%3C%2Fdiv%3E%20%3A%0A%20%20%20%20%20%20%20%20%20%20%3Ch1%20style%3D%7BheadingStyle%7D%3EEasy%20there%20pal%3C%2Fh1%3E%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%3C%2Fdiv%3E%0A%20%20%20%20)%3B%0A%20%20%7D%0A%7D%0A%0AReact.render(%3CInteractive%2F%3E%2C%20document.body)%3B&playground=true">
-              <S type="underline"><Text bold caps textColor="tertiary">Button Example</Text></S>
+          <Slide>
+            <Link target="_blank" href="https://github.com/zalmoxisus/redux-devtools-extension">
+              <Heading bold caps textColor="tertiary">Redux + Redux Devtools</Heading>
             </Link>
           </Slide>
-          <Slide align="flex-start" notes="show how the slides themselves are hot reloadable, show error messages by typo, etc" bgImage={images.reactHotLoader.replace("/", "")}>
-            <Link target="_blank" href="http://gaearon.github.io/react-hot-loader/">
-              <Text>Link</Text>
+          <Slide>
+            <Link target="_blank" href="https://github.com/kadirahq/react-storybook">
+              <Heading bold caps textColor="tertiary">React Storyboard</Heading>
             </Link>
           </Slide>
+          <Slide align="flex-start" bgImage={images.reactnative.replace("/", "")} />
         </Deck>
       </Spectacle>
     );
